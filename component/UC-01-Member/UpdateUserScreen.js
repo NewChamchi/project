687a52271189a2c972e4a05fed17b7763dec660b
NewChamchi <51375290+NewChamchi@users.noreply.github.com>
2022-11-21 13:59:30 +0900

    학교에서 작성

--- component/UC-01-Member/UpdateUserScreen.js
+++ component/UC-01-Member/UpdateUserScreen.js
@@ -25,13 +25,37 @@ const UpdateUserScreen = (props) => {
                 }}
             >
                 <Text style={{ fontSize: 20, paddingRight: 10 }}>
-                    이메일 :{" "}
+                    이름 변경:{" "}
                 </Text>
                 <TextInput
                     style={{
                         fontSize: 10,
                         borderWidth: 1,
-                        width: 250,
+                        width: "75%",
+                        paddingLeft: 10,
+                        marginRight: 10,
+                    }}
+                ></TextInput>
+                <TouchableOpacity>
+                    <MaterialCommunityIcons name="swap-horizontal" size={30} />
+                </TouchableOpacity>
+            </View>
+            <View
+                style={{
+                    flexDirection: "row",
+                    padding: 10,
+                    margin: 16,
+                    width: 300,
+                }}
+            >
+                <Text style={{ fontSize: 20, paddingRight: 10 }}>
+                    이메일 변경 :{" "}
+                </Text>
+                <TextInput
+                    style={{
+                        fontSize: 10,
+                        borderWidth: 1,
+                        width: "75%",
                         paddingLeft: 10,
                         marginRight: 10,
                     }}
